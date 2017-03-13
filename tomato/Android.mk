9e973f78a6b9184c9b00224cd11119db98d533dd
shrukul <shrukul99@gmail.com>
2017-03-13 19:41:54 +0530

    Revert "Update IMS blobs"
    This reverts commit 2e21abb72aac7f92a0ac6ca343554ca00ee20111.

--- tomato/Android.mk
+++ tomato/Android.mk
@@ -117,7 +117,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := imscmlibrary
 LOCAL_MODULE_OWNER := yu
 LOCAL_SRC_FILES := proprietary/framework/imscmlibrary.jar
-LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
